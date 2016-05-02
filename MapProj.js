07b52a07d9fffa3bdd986fe0d80bb8c1f82f68ae
GISJohnECS <GISJohnECS@Gmail.com>
2016-05-02 18:29:51 -0500

    chn

--- MapProj.js
+++ MapProj.js
@@ -122,15 +122,3 @@ function toggleLayers(i)
           }
         });
       }
-</script>
-<script>
-// Script to open and close the sidenav
-function w3_open() {
-    document.getElementsByClassName("w3-sidenav")[0].style.display = "block";
-    document.getElementsByClassName("w3-overlay")[0].style.display = "block";
-}
- 
-function w3_close() {
-    document.getElementsByClassName("w3-sidenav")[0].style.display = "none";
-    document.getElementsByClassName("w3-overlay")[0].style.display = "none";
-}
